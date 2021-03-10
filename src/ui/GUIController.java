0ffb08c8294fafdc996c712dd9c37834657ff687
unknown <seanquintero3@gmail.com>
2021-03-09 21:24:51 -0500

    Added second section pane, changed main anchor pane for a border pane, added menu to the top, on the center the first stage by default, and added functionality to the select section two button in menu

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -1,5 +1,62 @@
 package ui;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+
 public class GUIController {
+	
+	@FXML
+	private BorderPane mainBorderPane;
+	
+    @FXML
+    private MenuItem openSectionOne;
+
+    @FXML
+    private MenuItem openSectionTwo;
+
+    @FXML
+    private MenuItem openSectionThree;
+
+    @FXML
+    private MenuItem openSectionFour;
+
+    @FXML
+    void openSectionOne(ActionEvent event) {
+    	
+    }
+    
+    @FXML
+    void openSectionTwo(ActionEvent event) {
+    	try {
+    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/sectionTwoPane.fxml"));
+        	fxmlLoader.setController(this);
+
+        	Parent Pane = fxmlLoader.load();
+        
+        	mainBorderPane.setCenter(Pane);
+        	
+		} catch (IOException ioException) {
+			// TODO: handle exception with an alert that displays the content of the error.
+		}
+    	
+    }
+    
+    @FXML
+    void openSectionThree(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openSectionFour(ActionEvent event) {
+
+    }
+
+
 
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("fxml/sectionOnePane.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("fxml/mainPane.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Login");
