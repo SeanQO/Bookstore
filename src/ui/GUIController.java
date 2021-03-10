6f2dddf7e9099445cc8f7079a9df92712701cdef
unknown <seanquintero3@gmail.com>
2021-03-09 22:19:27 -0500

    Added comments in GUI controller

--- src/ui/GUIController.java
+++ src/ui/GUIController.java
@@ -1,7 +1,5 @@
 package ui;
-
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,12 +10,14 @@ import javafx.scene.layout.BorderPane;
 
 public class GUIController {
 	
+	// ****** Panes ******
 	@FXML
 	private BorderPane mainBorderPane;
 	
     @FXML
     private AnchorPane sectionOneAnchorPane;
 	
+    // ****** menu options ******
     @FXML
     private MenuItem openSectionOne;
 
@@ -29,7 +29,9 @@ public class GUIController {
 
     @FXML
     private MenuItem openSectionFour;
-
+    
+    
+    // ****** menu options actions ******
     @FXML
     void openSectionOne(ActionEvent event) {
     	mainBorderPane.setCenter(sectionOneAnchorPane);
@@ -71,15 +73,11 @@ public class GUIController {
     @FXML
     void openSectionFour(ActionEvent event) {
     	try {
-    		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/sectionFourPane.fxml"));
-        	fxmlLoader.setController(this);
+    		throw new IOException();
 
-        	Parent Pane = fxmlLoader.load();
-        
-        	mainBorderPane.setCenter(Pane);
-        	
 		} catch (IOException ioException) {
 			// TODO: handle exception with an alert that displays the content of the error.
+
 		}
     	
     }
