52898ad697d383ea0b7d3556ea01e6e26405b0e2
unknown <seanquintero3@gmail.com>
2021-03-11 19:36:38 -0500

    added alert for when the user tries to close the window, to avoid closing by mistake

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,18 @@
 package ui;
 
+import java.util.Optional;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
@@ -20,6 +28,29 @@ public class Main extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Login");
 		primaryStage.setResizable(false);
+		
+		//sets an alert for when the user tries to close the program window.
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			
+            public void handle(WindowEvent e) {
+            	Alert alert = new Alert(AlertType.CONFIRMATION);
+        		
+        		alert.setTitle("Exit confirmation");
+        		alert.setHeaderText("");
+        		alert.setContentText("Are you sure you want to exit the program?");
+        		
+        		Optional<ButtonType> result = alert.showAndWait();
+        		
+        		if (result.get() == ButtonType.OK) {
+        			Platform.exit();
+        	        System.exit(0);
+        		}else {
+        			e.consume();
+        		}
+            	
+            }
+        });
+		
 		primaryStage.show();
 
 	}
