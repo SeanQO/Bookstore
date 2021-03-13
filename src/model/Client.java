3f8df3af44826cd623c889c6cde453fc3d7fb4d0
BryanGF0822 <bryanicesi0822@gmail.com>
2021-03-13 17:41:30 -0500

    Sorting methods and their tests.
    The insertion, radix, merge ordering methods are added and tests are performed for each of these algorithms.

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,19 +1,21 @@
 package model;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Client {
 	ArrayList<Integer> isnbList;
 	Stack<Book> basket;
 	double timeInStore;
-	
+
 	public Client() {
 		isnbList = new ArrayList<Integer>();
 		basket = new Stack<Book>();
 		timeInStore = 0;
-		
+
 	}
-	
+
 	public ArrayList<Integer> getIsnbList() {
 		return isnbList;
 	}
@@ -21,37 +23,150 @@ public class Client {
 	public Stack<Book> getBasket() {
 		return basket;
 	}
-	
+
 	public double getTimeInStore() {
 		return timeInStore;
 	}
-	
+
 	public void addBookToBasket(Book book) {
 		basket.add(book);
-		
+
 	}
-	
+
 	public void addIsnbToList(int isnb) {
 		isnbList.add(isnb);
-		
+
 	}
-	
+
 	public void sortIsnbList(int sortingMethod) {
 		switch (sortingMethod) {
 		case 1:
+			insertionSortImperative(null);
 			break;
 		case 2:
+			mergeSort(null, sortingMethod);
 			break;
 		case 3:
+			radixSort(null);
 			break;
 
 		}
-		
+
 	}
-	
+
 	public void addTime(double time) {
-		timeInStore += time; 
+		timeInStore += time;
+	}
+
+	public static void insertionSortImperative(int[] input) {
+        for (int i = 1; i < input.length; i++) {
+            int key = input[i];
+            int j = i - 1;
+            while (j >= 0 && input[j] > key) {
+                input[j + 1] = input[j];
+                j = j - 1;
+            }
+            input[j + 1] = key;
+        }
+    }
+	
+	/*
+	public static int[] insertionSort(int[] nums) {
+
+		for (int i = 1; i < nums.length; i++) {
+			for (int j = i; j > 0 && nums[j - 1] > nums[j]; j--) {
+				int tmp = nums[j];
+				nums[j] = nums[j - 1];
+				nums[j - 1] = tmp;
+			}
+		}
+		return nums;
+	}
+	*/
+
+	public static void mergeSort(int[] a, int n) {
+		
+		if (n < 2) {
+			return;
+		}
+		int mid = n / 2;
+		int[] l = new int[mid];
+		int[] r = new int[n - mid];
+
+		for (int i = 0; i < mid; i++) {
+			l[i] = a[i];
+		}
+		for (int i = mid; i < n; i++) {
+			r[i - mid] = a[i];
+		}
+		mergeSort(l, mid);
+		mergeSort(r, n - mid);
+
+		merge(a, l, r, mid, n - mid);
+	}
+
+	public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+
+		int i = 0, j = 0, k = 0;
+		while (i < left && j < right) {
+			if (l[i] <= r[j]) {
+				a[k++] = l[i++];
+			} else {
+				a[k++] = r[j++];
+			}
+		}
+		while (i < left) {
+			a[k++] = l[i++];
+		}
+		while (j < right) {
+			a[k++] = r[j++];
+		}
 	}
 	
-	
+	public static void radixSort(int numbers[]) {
+        int maximumNumber = findMaximumNumberIn(numbers);
+
+        int numberOfDigits = calculateNumberOfDigitsIn(maximumNumber);
+
+        int placeValue = 1;
+
+        while (numberOfDigits-- > 0) {
+            applyCountingSortOn(numbers, placeValue);
+            placeValue *= 10;
+        }
+    }
+
+    private static void applyCountingSortOn(int[] numbers, int placeValue) {
+        int range = 10; // radix or the base
+
+        int length = numbers.length;
+        int[] frequency = new int[range];
+        int[] sortedValues = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            int digit = (numbers[i] / placeValue) % range;
+            frequency[digit]++;
+        }
+
+        for (int i = 1; i < range; i++) {
+            frequency[i] += frequency[i - 1];
+        }
+
+        for (int i = length - 1; i >= 0; i--) {
+            int digit = (numbers[i] / placeValue) % range;
+            sortedValues[frequency[digit] - 1] = numbers[i];
+            frequency[digit]--;
+        }
+
+        System.arraycopy(sortedValues, 0, numbers, 0, length);
+    }
+
+    private static int calculateNumberOfDigitsIn(int number) {
+        return (int) Math.log10(number) + 1; // valid only if number > 0
+    }
+
+    private static int findMaximumNumberIn(int[] arr) {
+        return Arrays.stream(arr).max().getAsInt();
+    }
+
 }
---
+++ test/model/ClientTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.jupiter.api.Test;
+
+public class ClientTest {
+
+	@Test
+    public void insertionSortTest() {
+        int[] input = {6, 2, 3, 4, 5, 1};
+        Client.insertionSortImperative(input);
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        assertArrayEquals("the two arrays are not equal", expected, input);
+    }
+	
+	@Test
+	public void mergeSortTest() {
+	    int[] actual = { 5, 1, 6, 2, 3, 4 };
+	    int[] expected = { 1, 2, 3, 4, 5, 6 };
+	    Client.mergeSort(actual, actual.length);
+	    assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void radizSortTest() {
+	    int[] numbers = {387, 468, 134, 123, 68, 221, 769, 37, 7};
+	    Client.radixSort(numbers);
+	    int[] numbersSorted = {7, 37, 68, 123, 134, 221, 387, 468, 769};
+	    assertArrayEquals(numbersSorted, numbers); 
+	}
+
+}
